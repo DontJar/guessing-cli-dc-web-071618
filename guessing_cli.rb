4d1e4b459ba4625e00568744a76ab808b85bd4d6
Jon Schulman <schulman99@gmail.com>
2018-07-10 00:08:10 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,14 +11,13 @@ require "pry"
 def run_guessing_game
   compy_guess = rand(1..6)
   fleshy_guess = nil
-  # until  fleshy_guess == "exit"
-
   puts "Guess a number between 1 and 6."
   fleshy_guess = gets.chomp
+    if fleshy_guess = "exit"
+      puts "Goodbye!"
     if fleshy_guess.to_i == compy_guess
       puts "You guessed the correct number!"
     else
       puts "The computer guessed #{compy_guess}."
     end
-# binding.pry
 end
