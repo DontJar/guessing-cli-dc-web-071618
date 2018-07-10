3e7b54e9e8c8e0d060cd3fbf4693522418981388
Jon Schulman <schulman99@gmail.com>
2018-07-10 00:01:34 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,5 +1,23 @@
 require "pry"
 
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+# puts "Enter your name:"
+# users_name = gets.chomp
+# puts say_hello(users_name)
+
+
 def run_guessing_game
+  compy_guess = rand(1..6)
+  fleshy_guess = nil
+  puts "Guess a number between 1 and 6"
+  fleshy_guess = gets.chomp
+    if fleshy_guess == compy_guess
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{compy_guess}."
+    end
+
 # binding.pry
 end
