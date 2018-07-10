d9570120cd0dde9b233df186e839b329547ad0fc
Jon Schulman <schulman99@gmail.com>
2018-07-10 00:35:40 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,6 +11,7 @@ require "pry"
 def run_guessing_game
   compy_guess = rand(1..6)
   fleshy_guess = nil
+  until fleshy_guess == "exit"
   puts "Guess a number between 1 and 6."
   fleshy_guess = gets.chomp
     if fleshy_guess == "exit"
@@ -20,4 +21,5 @@ def run_guessing_game
     else
       puts "The computer guessed #{compy_guess}."
     end
+  end
 end
